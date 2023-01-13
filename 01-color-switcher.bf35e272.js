0c5a050d1453b005e7103d00a96c9d3dc1fc9635
OlenaNov <109989618+OlenaNov@users.noreply.github.com>
2023-01-13 00:25:11 +0000

    Deploying to gh-pages from @ OlenaNov/goit-js-hw-09@d029a7736672b60770662b2f016907e6d6ed6d58 🚀

---
+++ 01-color-switcher.bf35e272.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");let n=null;function o(){e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){n=setInterval(o,1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(n),d.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.bf35e272.js.map
